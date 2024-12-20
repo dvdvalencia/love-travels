021987af0ed5f0e1f6a98a09980d53e38483c72c
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2024-12-20 01:38:24 -0500

    implementacion de imagen desertica en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-6 text-white bg-blue-500">
+    <footer className="py-6 text-black bg-bg-cover" style={{ backgroundImage: 'url(https://th.bing.com/th/id/OIP.tdxhElXNjHrjnxxzBJt5jQHaE7?w=626&h=417&rs=1&pid=ImgDetMain)' }}>
       <div className="text-center">
         <p className="text-sm">
           © {new Date().getFullYear()} Love Travels. Todos los derechos reservados.
