3a872cb8e12381589b482b206ec6330c10858f35
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2025-01-28 20:50:30 -0500

    informacion soobre lo que imcuira el plan

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,9 +1,17 @@
 const Plan = () => {
- return (
-<div className="p-4 bg-gray-800 border border-gray-600 rounded-lg bg-opacity-90">
-    <h1>El plan incluye</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla esse doloribus corporis itaque, perspiciatis illum excepturi. Vel ducimus quis, repudiandae, eaque mollitia dolor repellendus deserunt culpa, debitis consequuntur inventore! Consequatur!</p>
-</div>
- ); 
-}
-export default Plan
+  return (
+    <div className="w-11/12 p-4 mt-6 bg-black bg-opacity-50 rounded-lg md:w-2/3">
+      <div className="p-4 bg-gray-800 border border-gray-600 rounded-lg bg-opacity-90">
+        <h1>El plan incluye</h1>
+        <p>
+        TRANSPORTE: De turismo de acuerdo al número de personas con TV, baño, DVD, sillas reclinables, (busetas nuevas no traen baño). <br />
+ALOJAMIENTO:  Alojamiento seleccionado en coveñas sobre la primera linea del mar. <br />
+ALIMENTACIÓN: Desayuno y cena por noche de alojamiento (entran con cena y salen con desayuno), en Apartahotel Acapulco se obsequian los almuerzos. <br />
+TARJETA DE ASISTENCIA MÉDICA: Cobertura en caso de accidente enfermedad ocasionada por el viaje. <br />
+COORDINADOR ACOMPAÑANTE: durante los recorridos turísticos.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Plan;
