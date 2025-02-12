ec82921bd40c31521a5d298573881a8e4c82c7e3
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2025-02-11 23:10:54 -0500

    inicio implementacion de menu hamburguesa a partir de sm

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,18 +7,21 @@ import DropdownMenu from "./DropdownMenu";
 const NavBar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isInteriorOpen, setIsInteriorOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleDropdown = () => {
     isInteriorOpen && setIsInteriorOpen(false);
     setIsDropdownOpen(!isDropdownOpen);
   };
   
-
   const toggleInteriorDropdown = () => {
     if (isDropdownOpen) {
       setIsDropdownOpen(false); 
     }
     setIsInteriorOpen(!isInteriorOpen);
+  };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const interiorLocations = [
@@ -69,7 +72,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="fixed top-0 z-50 w-full h-40 text-white "
+    <div className="fixed top-0 z-50 w-full h-auto text-white "
       style={{ backgroundImage: 'url(https://th.bing.com/th/id/R.5587996ef551036d330197fc977c8fb8?rik=N5NC1d4AGjzTFg&riu=http%3a%2f%2fisclinical.com%2fmedia%2frevslider%2fSuperSerum%2fSS_BG.jpg&ehk=ZbZJcRrlkPesJMWjtIp45JqLS8yOG6pQskbipmtnmB0%3d&risl=&pid=ImgRaw&r=0)' }}
     >
       <nav className="flex flex-col items-center justify-between px-6 py-4 text-white">
@@ -79,22 +82,29 @@ const NavBar = () => {
           <img src="/love-travels-logo.ico" alt="Love Travels Logo" className="w-20 h-20" />
           </Link>
           <h1 className="flex-grow text-5xl font-bold text-center">LOVE TRAVELS</h1>
+          <button className="block ml-auto sm:hidden" onClick={toggleMenu}>
+            <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+            </svg>
+          </button>
         </div>
 
         {/* Navegación */}
-        <ul className="flex items-center mt-2 space-x-8">
-          <NavItem label="MAR" isOpen={isDropdownOpen} toggle={toggleDropdown}>
+        <ul className={`flex flex-col mt-2 space-y-2 sm:flex-row sm:space-y-0 sm:space-x-8 sm:mt-0 ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+        {/* <ul className={`flex ${isMenuOpen ? 'flex-col space-y-2' : ''} mt-2 sm:flex-row sm:space-y-0 sm:space-x-8 sm:mt-0`}> */}
+
+          <NavItem label="PLAYA Y MAR" isOpen={isDropdownOpen} toggle={toggleDropdown}>
             <DropdownMenu locations={seaLocations} customRoutes={customRoutes} />
           </NavItem>
 
-          <NavItem label="Interior de Colombia" isOpen={isInteriorOpen} toggle={toggleInteriorDropdown}>
+          <NavItem label="INTERIOR DE COLOMBIA" isOpen={isInteriorOpen} toggle={toggleInteriorDropdown}>
             <DropdownMenu locations={interiorLocations} customRoutes={customRoutes} />
           </NavItem>
 
           <li>
             <Link href="/contacto">
               <button className="px-8 py-2 text-lg text-white bg-green-600 rounded hover:bg-green-700">
-                Contacto
+                CONTACTO
               </button>
             </Link>
           </li>
